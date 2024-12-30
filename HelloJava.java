6c3b547955b2939d6234a71457d44b81eb541d09
Abdelrahman Atia <abdelrahmanatia745@gmail.com>
2024-12-30 13:58:17 +0200

    Add comments to methods in hellojava class

--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,10 @@
 
 public class HelloJava{
+	// this func prints hello hello java
     public void printHelloJava() {
         System.out.println("Hello Java");
     }
+	// this func prints helpMethod
 	public void helpMethod() {
     System.out.println("Help method");
 }
