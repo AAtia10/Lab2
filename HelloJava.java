48f654a69e0a9a61561780e3f3f576e88520dad4
Abdelrahman Atia <abdelrahmanatia745@gmail.com>
2024-12-30 13:46:41 +0200

    Add help method

--- HelloJava.java
+++ HelloJava.java
@@ -3,4 +3,8 @@ public class HelloJava{
     public void printHelloJava() {
         System.out.println("Hello Java");
     }
+	public void helpMethod() {
+    System.out.println("Help method");
+}
+
 }
