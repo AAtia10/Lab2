ee396b16dabcc0a1714fe0d750cc1bf26909d155
Abdelrahman Atia <abdelrahmanatia745@gmail.com>
2024-12-30 13:42:14 +0200

    Add comment to Helloiti class

--- HelloITI.java
+++ HelloITI.java
@@ -1,7 +1,9 @@
 public class HelloITI {
+	// this func prints hello iti
     public void printHelloITI() {
         System.out.println("Hello ITI");
     }
+	// this func prints hello hello git
 
     public void printHelloGit() {
         System.out.println("Hello Git");
